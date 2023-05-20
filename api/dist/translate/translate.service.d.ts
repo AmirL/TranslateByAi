1cf20d09912cbca6b08967dc0af0992311a81f88
Amir Latypov <Amir.Latypov@gmail.com>
2023-05-20 17:37:27 +0300

    Remove unused files

--- api/dist/translate/translate.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { MessagingService } from '../messaging-module/messaging.service';
-import { RequestTranslation } from './dto/request-translation.input';
-import { Translation } from 'common/translation.entity';
-export declare class TranslateService {
-    private readonly messagingService;
-    constructor(messagingService: MessagingService);
-    translate(input: RequestTranslation): Promise<Translation>;
-}
